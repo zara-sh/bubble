f325ce4931dbc3be35917f8b202c0b4a98547f47
Armaan Amazan <armaan.amazan@gmail.com>
2017-10-24 15:30:42 +0900

    preliminary routes and controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,31 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def search
+  end
+
+  def profile
+    @user = current_user
+  end
+
+  def edit # profile
+    @user = current_user
+  end
+
+  def update # profile
+    @user = current_user
+
+    if @user.update(user_params)
+      redirect_to profile_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phone, :photo, :bio, :hobbies)
+  end
 end
