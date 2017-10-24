53c7c015d5d78a098aacfa288988e229fcd6685c
Armaan Amazan <armaan.amazan@gmail.com>
2017-10-24 15:33:06 +0900

    commented out booking show page if necessary for later use

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :experiences do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:show]
+  # resources :bookings, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
