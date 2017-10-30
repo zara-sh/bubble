8afe4f7b7434dc3809f1c27da6da95ac57f1f38e
Becka Hsiao <rebeccaellicehsiao@gmail.com>
2017-10-30 22:02:38 +0900

    work more at home

---
+++ app/assets/javascripts/slick.js
@@ -0,0 +1,45 @@
+    $(document).ready(function(){
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+    arrows: true
+});
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+    });
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :categories
   has_attachment :photo
   # has_many :experience_categories, through: :experiences, source: :categories
-
   geocoded_by :current_sign_in_ip
 
     def self.find_for_facebook_oauth(auth)
