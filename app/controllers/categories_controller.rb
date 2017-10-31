e1cb452c5aba659477a1a2c481a892243a9279e2
zara-sh <zara.regist@gmail.com>
2017-10-31 10:02:08 +0900

    join table for schedule

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,28 +4,31 @@ class CategoriesController < ApplicationController
 
   def show
     @category= Category.find(params[:id])
-    @distance = 300000
+    @distance = 100
     @experiences = @category.experiences
+    @experience_in_cat = @category.experiences
 
+    #  if params[:date]
+    #   @experiences_in_cat = []
+    #   @category.experiences.each do |experience|
+    #     unless experience.schedules.where(date: params[:date]).empty?
+    #       @experiences_in_cat << experience.schedules.where(date: params[:date]).first.experience
+    #     end
+    #   end
+    # end
     if params[:date]
-      @experiences = []
-      @experiences_base_user.each do |experience|
-        unless experience.schedules.where(date: params[:date]).empty?
-          @experiences << experience.schedules.where(date: params[:date]).first.experience
-        end
-      end
+      @experiences = Experience.joins(:schedule).where(date: params[:date])
     end
 
-
-    #@date = params[:date]
     if current_user
-      # @experiences = @experiences.select {|item| item.latitude != nil}
-      @experiences = Experience.near([current_user.latitude, current_user.longitude], @distance)
-byebug
+      #@experiences = Experience.near([current_user.latitude, current_user.longitude], @distance)
+      @experiences = @experiences_in_cat.near([35, 139], @distance)
       @hash = Gmaps4rails.build_markers(@experiences) do |experience, marker|
           marker.lat experience.latitude
           marker.lng experience.longitude
         end
+
+
     else
       @experiences = @experiences.select {|item| item.latitude != nil}
       @hash = Gmaps4rails.build_markers(@experiences) do |experience, marker|
@@ -33,6 +36,8 @@ byebug
         marker.lng experience.longitude
       end
     end
+
+
       # marker.infowindow render_to_string(partial: "/experiences/map_box", locals: { experience: experience })
   end
 
