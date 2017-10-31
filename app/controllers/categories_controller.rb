767e40d92a3c10179a953f745546fc72b9213fe5
zara-sh <zara.regist@gmail.com>
2017-10-31 12:15:08 +0900

    still in progress for lat and lomg

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,11 +13,12 @@ class CategoriesController < ApplicationController
       @experiences = @category.experiences.joins(:schedules).where(schedules: {date: params[:date]})
     end
 
+byebug
 
     if current_user
       #@experiences = Experience.near([current_user.latitude, current_user.longitude], @distance)
       @all_experiences = @experiences.where.not(latitude: nil, longitude: nil)
-      @experiences_in_map = @all_experiences.near([current_user.latitude, current_user.longitude], @distance)
+      @experiences_in_map = @all_experiences.near([35, 139], @distance)
       @hash = Gmaps4rails.build_markers(@experiences_in_map) do |experience, marker|
           marker.lat experience.latitude
           marker.lng experience.longitude
