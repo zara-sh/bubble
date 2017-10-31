bd888ab72e9de0bcc66d84330d985b7361f5d69d
Becka Hsiao <rebeccaellicehsiao@gmail.com>
2017-10-31 17:33:30 +0900

    made several pages responsive

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -6,6 +6,7 @@
   fade: true,
   asNavFor: '.slider-nav'
 });
+
 $('.slider-nav').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
@@ -16,8 +17,57 @@ $('.slider-nav').slick({
     arrows: true
 });
 
+ $('.slider-for-less').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav-less'
+});
 
-$('.center').slick({
+$('.slider-nav-less').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for-less',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+    arrows: true
+});
+
+    });
+
+
+
+   $(window).on('resize load', function () {
+          if ( $(window).width() < 739) {
+    $('.center').slick({
+  centerMode: true,
+  centerPadding: '40px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 319,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 300,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+    } else {
+          $('.center').slick({
   centerMode: true,
   centerPadding: '60px',
   slidesToShow: 2,
@@ -42,5 +92,9 @@ $('.center').slick({
     }
   ]
 });
+      }
 
-    });
+
+   });
+
+
