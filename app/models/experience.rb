d1ac862a1d42f9ba87c0cb1ee062d64dae4c8526
zara-sh <zara.regist@gmail.com>
2017-11-01 16:52:54 +0900

    update seed

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -5,7 +5,14 @@ class Experience < ApplicationRecord
   has_many :schedules
   validates :title, :description, :location, :incentive, presence: :true
   has_attachments :photos, maximum: 5
+<<<<<<< Updated upstream
 
+=======
+  validates :title, presence: true,length: {maximum: 250}, on: :create, allow_nil: false
+  validates :description, presence: true,length: {maximum: 10000}, on: :create, allow_nil: false
+  validates :location, presence: true,length: {maximum: 100}, on: :create, allow_nil: false
+  validates :incentive, presence: true,length: {maximum: 100}, on: :create, allow_nil: false
+>>>>>>> Stashed changes
   geocoded_by :location
   after_validation :geocode, if: :location_changed?
   #AVABILITY = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
