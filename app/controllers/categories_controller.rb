20ea188546cd1944134cf5740630da96fe471f89
zara-sh <zara.regist@gmail.com>
2017-11-01 09:42:37 +0900

    fix filter by location

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,14 +18,14 @@ class CategoriesController < ApplicationController
     if current_user
       #@experiences = Experience.near([current_user.latitude, current_user.longitude], @distance)
       @all_experiences = @experiences.where.not(latitude: nil, longitude: nil)
-      @experiences_in_map = @all_experiences.near([current_user.latitude, current_user.longitude], @distance)
-      @hash = Gmaps4rails.build_markers(@experiences_in_map) do |experience, marker|
+      @experiences = @all_experiences.near([current_user.latitude, current_user.longitude], @distance)
+      @hash = Gmaps4rails.build_markers(@experiences) do |experience, marker|
           marker.lat experience.latitude
           marker.lng experience.longitude
         end
     else
-      @all_experiences = @experiences.where.not(latitude: nil, longitude: nil)
-      @hash = Gmaps4rails.build_markers(@all_experiences) do |experience, marker|
+      @experiences = @experiences.where.not(latitude: nil, longitude: nil)
+      @hash = Gmaps4rails.build_markers(@experiences) do |experience, marker|
         marker.lat experience.latitude
         marker.lng experience.longitude
       end
