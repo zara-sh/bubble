c3114d6d773ade2cf91183a7adb637a394a2fab4
Becka Hsiao <rebeccaellicehsiao@gmail.com>
2017-11-02 17:55:56 +0900

    finished fixing event

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :experience
   belongs_to :schedule
+  has_many :events, dependent: :destroy
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,14 +1,14 @@
 class Experience < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :bookings
-  has_many :schedules
+  has_many :bookings, dependent: :destroy
+  has_many :schedules, dependent: :destroy
   has_attachments :photos, maximum: 5
 
-  validates :title, presence: true,length: {maximum: 1000}, on: :create, allow_nil: false
+  validates :title, presence: true,length: {maximum: 20}, on: :create, allow_nil: false
   validates :description, presence: true,length: {maximum: 10000}, on: :create, allow_nil: false
   validates :location, presence: true,length: {maximum: 100}, on: :create, allow_nil: false
-  validates :incentive, presence: true,length: {maximum: 100}, on: :create, allow_nil: false
+  validates :incentive, presence: true,length: {maximum: 20}, on: :create, allow_nil: false
 
 #   validates :title, presence: true,length: {maximum: 25}, on: :create, allow_nil: false
 #   validates :description, presence: true,length: {maximum: 255}, on: :create, allow_nil: false
